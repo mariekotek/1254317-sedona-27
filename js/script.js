5f333680300931cfeb41608398b6755fdc398ac4
marietsar <marietsar@users.noreply.github.com>
2020-03-01 20:31:22 +0500

    Исправления в JS

--- js/script.js
+++ js/script.js
@@ -2,31 +2,46 @@ var button = document.querySelector(".search-hotel-button");
 var popup = document.querySelector(".search-form");
 var close = document.querySelector(".search-hotel-button");
 var form = popup.querySelector("form");
-var arriveDate = popup.querySelector("[date]");
+var arrival = popup.querySelector(".arrival");
+var departure = popup.querySelector(".departure");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+    storage = localStorage.getItem("login");
+  } catch (err) {
+    isStorageSupport = false;
+  }
 
 button.addEventListener("click", function (evt) {
 	evt.preventDefault();
-	popup.classList.add("form-show");
-	arriveDate.focus();
- });
-
-close.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.remove("form-show");
+	popup.classList.toggle("form-show");
+  if (storage) {
+      arrival.value = storage;
+      departure.focus();
+    } else {
+      arrival.focus();
+    }
  });
 
 form.addEventListener("submit", function (evt) {
-    if (!arrial-date || !departure-date.value) {
+    if (!arrival.value || !departure.value) {
       evt.preventDefault();
+      popup.offsetWidth = popup.offsetWidth;
       console.log("Введите даты");
+    } else {
+      if (isStorageSupport) {
+        localStorage.setItem("arrival", arrial.value);
+      }
     }
   });
  
  window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
       evt.preventDefault();
-      if (popup.classList.contains("modal-show")) {
-        popup.classList.remove("modal-show");
+      if (popup.classList.contains("form-show")) {
+        popup.classList.remove("form-show");
       }
     }
   });
